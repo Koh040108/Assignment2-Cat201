c7c24ec00ff69a86247e803c6ab3cc31aebef6d7
Koh Kean Hoe <keanhoekoh@student.usm.my>
2024-12-22 16:00:15 +0800

    Initial commit

---
+++ src/pages/TouristSpots.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TouristSpots = () => (
+    <section id="tourist-spots">
+        <h2>Top Tourist Spots in Penang</h2>
+        <div>
+            <h3>Kek Lok Si Temple</h3>
+            <img src="kek-lok-si.jpg" alt="Kek Lok Si Temple" />
+            <p>A beautiful Buddhist temple in Penang.</p>
+        </div>
+        {/* Add more spots */}
+    </section>
+);
+
+export default TouristSpots;
