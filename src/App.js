2d3589df5c08d21c09e311136ac8bcaa15d30166
AVEENATH <aveenath21@gmail.com>
2024-12-30 14:48:01 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import Home from './pages/Home';
+import React from 'react';
+import Home from './pages/Home'; // Ensure this is the correct Home component path
 import Header from './components/Header';
 import Footer from './components/Footer';
-// Add other imports here
+import TouristSpots from "./pages/TouristSpots";
+import Culture from "./pages/Culture";
+// Add other imports here as needed
 
 function App() {
-  return (
-      <div>
-        <Header />
-        <main>
-          <Home />
-          {/* Add other components */}
-        </main>
-        <Footer />
-      </div>
-  );
+    return (
+        <div>
+            <Header />
+            <main>
+                {/* Main sections of the site */}
+                <Home />
+                <TouristSpots />
+                <Culture />
+                {/* Add other components or pages here */}
+            </main>
+            <Footer />
+        </div>
+    );
 }
+
 export default App;
